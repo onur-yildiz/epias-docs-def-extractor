4e8d9ee843afc032b3006be2e16389d9553270d9
Onur YILDIZ <oyildiz@smartpulse.io>
2023-04-18 08:56:29 +0300

    initial

---
+++ index.ts
@@ -0,0 +1,109 @@
+import fetch from "node-fetch";
+import fs from "fs";
+import { JSDOM } from "jsdom";
+
+const OBJECT_TYPE = "struct";
+const TARGET_URL = "https://gunici-prp.epias.com.tr/gunici-service/technical/tr/index.html";
+const OUTPUT_DIRECTORY_PATH = "outputs";
+const OUTPUT_FILE_PATH = `${OUTPUT_DIRECTORY_PATH}/output_${new Date().getTime()}.cs`;
+
+const app = async () => {
+    if(!fs.existsSync(OUTPUT_DIRECTORY_PATH)) fs.mkdirSync(OUTPUT_DIRECTORY_PATH);
+
+    const response = await fetch(TARGET_URL);
+    console.log("FETCHED URL");
+
+    const htmlString = await response.text();
+
+    const dom = new JSDOM(htmlString);
+    const document = dom.window.document;
+
+    const sect1s = document.querySelectorAll("div.sect1");
+
+    const definitionsSect = [...sect1s.values()].find((sect) => sect.querySelector("#_definitions"));
+    const definitions = definitionsSect.querySelectorAll(".sectionbody .sect2");
+
+    for (const definition of definitions) {
+        const properties = [];
+
+        const table = definition.querySelector("table");
+        if (table === null) continue;
+        Array.from(table.tBodies[0].rows).forEach((row) => {
+            if (row.cells.length === 3) {
+                const name = row.cells[0].querySelector("strong").textContent.trim();
+                const isOptional = row.cells[0].querySelector("em").textContent.trim() === "opsiyonel";
+                const description = row.cells[1].textContent.trim().replace("\n", " ");
+                const type = typeConverter(row.cells[2].textContent.trim(), name);
+                properties.push({ name, isOptional, description, type });
+            } else {
+                const name = row.cells[0].querySelector("strong").textContent.trim();
+                const isOptional = row.cells[0].querySelector("em").textContent.trim() === "opsiyonel";
+                const type = typeConverter(row.cells[1].textContent.trim(), name);
+                properties.push({ name, isOptional, type });
+            }
+        });
+
+        const className = definition.querySelector("h3").textContent.split(" ").at(-1).trim();
+        const classCode = `public ${OBJECT_TYPE} ${className} {\n${properties
+            .map(
+                (prop) =>
+                    `  public ${prop.type}${prop.isOptional && prop.type !== "string" ? "?" : ""} ${
+                        prop.name
+                    } { get; set; } // ${prop.description}`
+            )
+            .join("\n")}\n}\n\n`;
+
+        // console.log(classCode);
+        fs.appendFileSync(OUTPUT_FILE_PATH + "", classCode);
+    }
+};
+
+const typeConverter = (type: string, propName: string) => {
+    const arrayTypeRegex = /<(.+)> array/;
+    const arrayTypeMatch = type.match(arrayTypeRegex);
+    if (arrayTypeMatch && arrayTypeMatch.length > 0) {
+        let typeString = arrayTypeMatch[1];
+
+        if (typeString.includes("enum (")) return createAndGetEnum(typeString, propName);
+        return `${getKnownType(typeString).replace(" ", "")}[]`;
+    }
+
+    if (type.includes("enum (")) return createAndGetEnum(type, propName);
+
+    return getKnownType(type);
+};
+
+const getKnownType = (type: string) => {
+    switch (type) {
+        case "string (date-time)":
+            return "DateTime";
+        case "integer (int64)":
+            return "long";
+        case "integer (int32)":
+            return "int";
+        case "number":
+            return "decimal";
+        case "boolean":
+            return "bool";
+        default:
+            return type;
+    }
+};
+
+const createAndGetEnum = (enumString: string, propName: string) => {
+    const enumTypeRegex = /\((.+)\)/;
+    const enumTypeMatch = enumString.match(enumTypeRegex);
+    if (enumTypeMatch && enumTypeMatch.length > 0) {
+        const enums = enumTypeMatch[1].split(",").map((s) => s.trim());
+
+        const enumName = capitalizeFirstLetter(propName);
+        const classCode = `public enum ${enumName} {\n${enums.map((e) => `${e},`).join("\n")}\n}\n\n`;
+
+        fs.appendFileSync(OUTPUT_FILE_PATH, classCode);
+        return enumName;
+    }
+};
+
+const capitalizeFirstLetter = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
+
+app();
